d6f10d4f61cd0d6d1e4e3f4d7cc1e545a26f210d
Andre Araujo <araujo.a@me.com>
2023-04-07 10:13:30 -0600

    initial commit

--- blog/pages/index.tsx
+++ blog/pages/index.tsx
@@ -1,6 +1,9 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Blog() {
+  const origin = "http://localhost:3000";
+
+  const fullURL = `${origin}/about`;
   return (
     <div>
       <h3>This is our blog</h3>
@@ -12,7 +15,14 @@ export default function Blog() {
           <Link href="/post/2">Post 2</Link>
         </li>
       </ul>
-      <Link href="/">Home</Link>
+      <Link href="/">Home Do not work</Link>
+      <br />
+      <Link href="/about">About page - do not work</Link>
+      <br />
+      <Link href="../about">About page - do not work</Link>
+      <br />
+      <Link href={fullURL}>About page - do not work</Link>
+      <br />
     </div>
-  )
+  );
 }
